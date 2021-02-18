1cb79cde946ae01ab871d98bfb37817c310067b5
Victor <v.g.m_@hotmail.com>
2021-02-18 12:53:56 +0100

    Lazy Load implementado

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const rutas: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'listado',
+        component: ListadoComponent,
+      },
+      {
+        path: 'agregar',
+        component: AgregarComponent,
+      },
+      {
+        path: 'editar/:id',
+        component: AgregarComponent,
+      },
+      {
+        path: ':id',
+        component: HeroeComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'listado'
+      }
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forChild( rutas)],
+  exports: [
+    RouterModule
+  ]
+})
+export class HeroesRoutingModule {}
